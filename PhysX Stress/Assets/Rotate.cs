23f7a8df391bff8c20770c292f14939c2ae0071c
Void Chicken <acronstudios@gmail.com>
2016-11-22 23:15:27 -0800

    Made camera rotate.

---
+++ PhysX Stress/Assets/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+    public float speed;
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(Mathf.Sin(Time.time* speed), 0, Mathf.Cos(Time.time * speed))*19;
+        transform.LookAt(Vector3.zero);
+	}
+}
